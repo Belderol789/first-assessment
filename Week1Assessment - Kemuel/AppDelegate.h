47dd1632c3a74c808b228d34fec11386675133e7
Kemuel Clyde Belderol <Belderol789@kemuels-mac.local>
2017-03-19 19:50:34 -0700

    Initial Commit

---
+++ Week1Assessment - Kemuel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Week1Assessment - Kemuel
+//
+//  Created by Kemuel Clyde Belderol on 19/03/2017.
+//  Copyright © 2017 Burst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Week1Assessment - Kemuel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Week1Assessment - Kemuel
+//
+//  Created by Kemuel Clyde Belderol on 19/03/2017.
+//  Copyright © 2017 Burst. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
